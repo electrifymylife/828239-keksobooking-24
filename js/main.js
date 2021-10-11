64239cd53ce2c3edced3951ae9d69e59690d21ea
g.vihrova <g.vihrova@advance.ru>
2021-10-11 18:55:36 +0300

    Генерация данных, правки

--- js/main.js
+++ js/main.js
@@ -1,17 +1,3 @@
-const getRandomPositiveFloat = (a, b, digits = 1) => {
-  const lower = Math.min(Math.abs(a), Math.abs(b));
-  const upper = Math.max(Math.abs(a), Math.abs(b));
-  const result = Math.random() * (upper - lower) + lower;
-  return result.toFixed(digits);
-};
-
-const getRandomPositiveInteger = (a, b) => {
-  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
-  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
-};
-
 const ADS_COUNT = 10;
 const authorAvatar = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10'];
 const offerType = ['palace', 'flat', 'house', 'bungalo'];
@@ -24,20 +10,35 @@ const photos = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
 ];
 
-const getRandomArrayElement = (elements) => {
-  return elements[getRandomPositiveInteger(0, elements.length - 1)];
+const getRandomPositiveFloat = (firstNum, secondNum, digits = 1) => {
+  const lower = Math.min(Math.abs(firstNum), Math.abs(secondNum));
+  const upper = Math.max(Math.abs(firstNum), Math.abs(secondNum));
+  const result = Math.random() * (upper - lower) + lower;
+  return result.toFixed(digits);
 };
+
+const getRandomPositiveInteger = (firstNum, secondNum) => {
+  const lower = Math.ceil(Math.min(Math.abs(firstNum), Math.abs(secondNum)));
+  const upper = Math.floor(Math.max(Math.abs(firstNum), Math.abs(secondNum)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
 
 const createAd = (quantity) => {
   const ads = [];
+  // eslint-disable-next-line id-length
   for (let i=0; i < quantity; i++) {
-    const ad = {
+    const lat = getRandomPositiveFloat(35.65000, 35.70000, 5);
+    const lng = getRandomPositiveFloat(139.70000, 139.80000, 5);
+    ads[i] = {
       author: {
-        avatar: 'img/avatars/user' + authorAvatar[i] + '.png',
+        avatar: `img/avatars/user${authorAvatar[i]}.png`,
       },
       offer: {
         title: 'Объявление',
-        address: getRandomPositiveFloat(35.65000, 35.70000, 5) + ', ' + getRandomPositiveFloat(139.70000, 139.80000, 5),
+        address: `${lat}, ${lng}`,
         price: getRandomPositiveInteger(1000, 10000),
         type: getRandomArrayElement(offerType),
         rooms: getRandomPositiveInteger(1, 10),
@@ -48,8 +49,11 @@ const createAd = (quantity) => {
         description: 'Описание помещения',
         photos: getRandomArrayElement(photos),
       },
+      location: {
+        lat: lat,
+        lng: lng,
+      },
     };
-    ads[i] = ad;
   }
   return ads;
 };
